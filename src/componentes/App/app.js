e7458bdd704fd105e6fb80dd1ba89bcf8159cbf3
André Felipe <academicworks.adf@gmail.com>
2019-11-10 18:01:08 -0300

    Até o passo 6

--- src/componentes/App/app.js
+++ src/componentes/App/app.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { DataTable } from '../DataTable/dataTable';
 import './app.css';
-import { AppBar, Toolbar, Typography, Card, CardContent } from '@material-ui/core';
+import { Card, CardContent } from '@material-ui/core';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import red from '@material-ui/core/colors/red';
 import FirebaseService from '../../services/firebaseServices';

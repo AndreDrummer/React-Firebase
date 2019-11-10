dd2decf5884a12eaf48393f3674f416c6c60aa11
André Felipe <academicworks.adf@gmail.com>
2019-11-10 18:00:19 -0300

    Até o passo 6

--- src/componentes/App/app.js
+++ src/componentes/App/app.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import { DataTable } from '../DataTable/dataTable';
 import './app.css';
-import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, Toolbar, Typography, Card, CardContent } from '@material-ui/core';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import red from '@material-ui/core/colors/red';
+import FirebaseService from '../../services/firebaseServices';
+import { urls } from '../../utils/urlUtils';
+import  { Add } from '../Add/add';
+import  { Welcome } from '../Welcome/welcome';
+import { Route } from 'react-router-dom';
+import { TopBar } from '../App/topBar';
 
 const theme = createMuiTheme({
   palette: {
@@ -12,31 +18,42 @@ const theme = createMuiTheme({
 });
 
 class App extends Component {
-
   state = {
-    data: [
-      {
-        key: 'teste key key',
-        temperature: 'test temperature',
-        umidade: 'teste de humidade',
-        cliente: 'teste client',
-        data: 'teste key data'
-      }
-    ]
+    data: []
+  }
+
+  componentDidMount() {
+    FirebaseService.getDataList('leituras', (dataReceived) => {
+      this.setState({data: dataReceived})
+    })
   }
 
   render() {
     return (
       <MuiThemeProvider theme={theme}>
         <React.Fragment>
-          <AppBar position="static">
-            <Toolbar>
-              <Typography type="title" color="inherit">
-                My Awesome React App
-              </Typography>
-            </Toolbar>
-          </AppBar>
-          <DataTable data={this.state.data}/>
+        <TopBar/>
+          {/* <DataTable data={this.state.data}/> */}
+          <Card style={{margin: '50px'}}>
+            <CardContent>
+
+              <Route exact
+                path={urls.home.path}
+                render={(props) => <Welcome {...props}/>}
+              />
+              <Route exact
+                path={urls.data.path}
+                render={(props) => 
+                <DataTable {...props} data={this.state.data}/>}
+              />              
+
+              <Route exact
+                path={urls.add.path}
+                render={(props) =>
+                  <Add {...props}/>}
+              />
+            </CardContent>            
+          </Card>
         </React.Fragment>
       </MuiThemeProvider>
     );
--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './componentes/App/app';
 import * as serviceWorker from './serviceWorker';
+import { urls } from './utils/urlUtils';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <Route path={urls.home.path} component={App}/>
+    </Router>,
+    document.getElementById('root')
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
